1cc1c3aaa9956421ddba22794abc8fdf0ce9d782
ramswarooppatidar <ramkatariya1603@gmail.com>
2024-08-01 19:40:37 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Home from "./components/Home";
 import Sidebar from "./components/Sidebar";
+import {createHashRouter, RouterProvider, createBrowserRouter} from "react-router-dom"
 
 function App() {
+  // for hosting  ceateHashRouter
+  const router = createHashRouter([
+    {
+      path:'/',
+      element: <Home />
+    }
+  ])
+  // const router = createBrowserRouter([
+  //   {
+  //     path:'/chatApp',
+  //     element: <Home />
+  //   }
+  // ])
   return (
     <>
+
+    <RouterProvider router={router} />
     {/* <h1>welcom to sidebar !</h1> */}
-    <Home/>
+    {/* <Home/> */}
     </>
   );
 }
